b971ba37da0ade6adac1f0616ee5f6bf2a4899cd
Harmun ROSSOUW <hrossouw@c2r1s4.wethinkcode.co.za>
2018-05-22 13:59:07 +0200

    strncpy v1.1

--- SRC/ft_strncpy.c
+++ SRC/ft_strncpy.c
@@ -6,18 +6,17 @@
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 15:01:04 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/05/21 15:40:56 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/05/22 12:12:12 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> //test
 
-char	*ft_strncpy(char *dst, char *src, int len)
+char	*ft_strncpy(char *dst, const char *src, unsigned int len)
 {
 	int i;
 
 	i = 0;
-	len = 2;
 	while (src[i] != '\0' && i < len)
 	{
 		dst[i] = src[i];
@@ -29,11 +28,8 @@ char	*ft_strncpy(char *dst, char *src, int len)
 
 int		main(int argc, char **argv) //test
 {
-	if (argc == 4)
-	{
 		printf("1: %s\n", argv[1]);
 		printf("2: %s\n", argv[2]);
-		printf("Result = %s", ft_strncpy(argv[1], argv[2], argv[3])); 
-	}
+		printf("Result = %s", ft_strncpy(argv[1], argv[2], 2)); 
 	return (0);
 }
