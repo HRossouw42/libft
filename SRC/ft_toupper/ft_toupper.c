448b3464a2de3bd7739060ac36b0e7b370ae5297
Harmun ROSSOUW <hrossouw@c2r1s4.wethinkcode.co.za>
2018-05-23 15:13:15 +0200

    toupper

--- SRC/ft_toupper/ft_toupper.c
+++ SRC/ft_toupper/ft_toupper.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/23 14:01:16 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/05/23 14:05:15 by hrossouw         ###   ########.fr       */
+/*   Created: 2018/05/23 14:48:01 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/05/23 15:08:36 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalnum (int c)
+#include <stdio.h> //test
+
+int		ft_toupper(int c)
 {
-	if (c >= '0' && c <= 'z')
-		return (c);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
 	else
-		return (0);
+		return (c);
+}
+
+int		main(void) //test
+{
+	printf("%c", ft_toupper('a'));
+	return (0);
 }
