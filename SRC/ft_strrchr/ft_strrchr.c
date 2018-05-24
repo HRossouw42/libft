bc58d055e08d6eaea25aac1d6f5447715fa3fbca
Harmun ROSSOUW <hrossouw@c2r1s4.wethinkcode.co.za>
2018-05-24 16:33:27 +0200

    strrchr UNTESTED

---
+++ SRC/ft_strrchr/ft_strrchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/24 15:53:10 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/05/24 15:55:32 by hrossouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+const char		*ft_strrchr(const char *s, int c)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = ft_strlen(s);
+	if (s == 0)
+		return (0);
+	if ((c > 255) || (c < 0))
+	{
+		return (0);
+	}
+	while (i > len)
+	{
+		if ((char)c == s[i])
+			return ((const char*)&s[i]);
+		len--;
+	}
+	return (0);
+}
