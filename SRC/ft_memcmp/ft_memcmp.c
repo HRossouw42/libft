c34988c756c0bae0a2b878abaecce8bf62a7a6b5
Harmun ROSSOUW <hrossouw@c2r3s4.wethinkcode.co.za>
2018-05-26 14:28:28 +0200

    memcmp, strdupUNTESTED

---
+++ SRC/ft_memcmp/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/26 13:55:10 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/05/26 14:18:37 by hrossouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int		ft_memcmp(const void *b1, const void *b2, size_t len)
+{
+	unsigned char	*buffer_1;
+	unsigned char	*buffer_2;
+	size_t			i;
+
+	buffer_1 = (unsigned char *)s1;
+	buffer_2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (buffer_1[i] != buffer_2[i])
+			return (buffer_1[i] - buffer_2[i]);
+		i++;
+	}
+	return (0);
+}
