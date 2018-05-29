4f84a9a5e4c621cc620344f2763288e9a990c68e
Harmun ROSSOUW <hrossouw@c2r3s4.wethinkcode.co.za>
2018-05-29 11:33:41 +0200

    putendl

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,11 +6,11 @@
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 11:14:59 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/05/29 11:20:18 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/05/29 11:33:03 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>>
+#include "libft.h"
 
 void	ft_putchar(char c)
 {
--- ft_putendl.c
+++ ft_putendl.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_putendl.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/29 11:20:50 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/05/29 11:32:50 by hrossouw         ###   ########.fr       */
+/*   Created: 2018/05/29 11:28:59 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/05/29 11:32:35 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr(char const *s)
+void	ft_putendl(char const *s)
 {
 	if (s != 0)
-		while (*s)
-		{
-			ft_putchar(*s)
-				s++;
-		}
+	{
+		ft_putstr(s);
+		ft_putchar('\n');
+	}
 }
