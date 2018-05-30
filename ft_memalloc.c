ce89f89cf3718d655733e84b2c5c018b59444f6b
Harmun ROSSOUW <hrossouw@c2r3s4.wethinkcode.co.za>
2018-05-30 16:53:48 +0200

    memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,37 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/29 11:36:29 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/05/30 16:34:54 by hrossouw         ###   ########.fr       */
+/*   Created: 2018/05/30 12:11:56 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/05/30 16:43:07 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int n) 402
+void	*ft_memalloc(size_t size)
 {
-	if (n > 2147483647 || n < -2147483648)
-		return (0);
-	if (n == -2147483648) //dont make my mistakes, fix me senpai
+	void	*buffer;
+
+	buffer = malloc(size);
+	if (buffer)
 	{
-		ft_putstr("-2147483648");
-		return (0);
+		ft_bzero(buffer, size);
+		return (buffer);
 	}
-	if (n < 0)
-	{
-		n = n * -1;
-		ft_putchar('-');
-	}
-	if (n < 10)
-	{
-		ft_putchar(n + '0');
-		return (0);
-	}
-	ft_putnbr(n / 10);
-	ft_putchar((n % 10) + '0');
 	return (0);
 }
