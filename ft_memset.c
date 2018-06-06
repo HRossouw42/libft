29a94c9672e2a61307d9012d505e08ef3e33b869
Harmun ROSSOUW <hrossouw@c2r3s4.wethinkcode.co.za>
2018-06-06 14:38:10 +0200

    memset fixed

--- ft_memset.c
+++ ft_memset.c
@@ -6,20 +6,20 @@
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 12:48:43 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/06/05 10:54:16 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/06/06 13:22:47 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t n)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*buffer;
 	size_t			i;
 
 	buffer = b;
 	i = 0;
-	while (i < n)
+	while (i < len)
 	{
 		buffer[i] = (unsigned char)c;
 		i++;
