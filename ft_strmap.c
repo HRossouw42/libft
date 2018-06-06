7f22f610b6b01516dd5abbf1302f94f496c29e80
Harmun ROSSOUW <hrossouw@c2r3s4.wethinkcode.co.za>
2018-06-06 11:28:40 +0200

    strmap fixed

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: hrossouw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 18:14:15 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/06/01 10:13:30 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/06/06 11:27:58 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	unsigned int	len;
 
 	if (!s)
-		return (0);
+		return (NULL);
 	len = ft_strlen(s);
 	new_string = ft_strnew(len);
+	if (new_string = NULL)
+		return (NULL);
 	i = 0;
 	while (i < len)
 	{
